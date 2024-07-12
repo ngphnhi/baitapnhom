ca8b8182e7f56c124e39752243571c69e2f07fa9
ngphnhi <166349664+ngphnhi@users.noreply.github.com>
2024-07-12 07:57:20 +0700

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,80 +1,79 @@
-import React from 'react'
+import React from 'react';
 import instagramLogo from '../assets/instagram.svg';
 import facebookLogo from '../assets/facebook.svg';
 import twitterLogo from '../assets/twitter.svg';
 import youtubeLogo from '../assets/youtube.svg';
 
-
 const Footer = () => {
   return (
-    <footer className="mt-5 bg-orange-100 ">
-    <div className="container mx-auto">
-      <div className="flex flex-col md:flex-row py-5">
-        <div className="md:w-1/3 flex flex-col items-end justify-center text-right md:pr-3">
-          <h2 className="text-2xl md:text-3xl font-bold uppercase">
-            Join Our<br />Mailing List
-          </h2>
+    <footer className="mt-5 bg-custom">
+      <div className="container">
+        <div className="row py-5">
+          <div className="col-4 d-flex flex-column align-items-end justify-content-center text-end">
+            <h2 className="text-uppercase">
+              join our<br />mailing list
+            </h2>
+          </div>
+          <div className="col-8 d-flex align-items-center pr-3">
+            <form className="w-75">
+              <div className="input-group">
+                <input 
+                  type="email" 
+                  className="form-control text-uppercase no-border" 
+                  placeholder="your email address" 
+                  aria-label="Email Address" 
+                />
+                <button 
+                  className="btn btn-custom text-uppercase no-border ms-2" 
+                  type="submit"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="md:w-2/3 flex items-center pr-3">
-          <form className="w-full">
-            <div className="flex">
-              <input
-                type="email"
-                className="form-input w-full text-gray-900 uppercase border-none pl-2"
-                placeholder="Your email address"
-                aria-label="Email Address"
-              />
-              <button
-                className="btn bg-blue-600 text-white uppercase ml-2 py-2 px-4 rounded"
-                type="submit"
-              >
-                Subscribe
-              </button>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 d-flex flex-column align-items-center">
+            <p className="lead text-uppercase">connect</p>
+            <div className="social-links">
+              <a href="#" className="social-link">
+                <img src={instagramLogo} alt="Instagram" />
+                <span className="social-text">Instagram</span>
+              </a>
+              <a href="#" className="social-link">
+                <img src={facebookLogo} alt="Facebook" />
+                <span className="social-text">Facebook</span>
+              </a>
+              <a href="#" className="social-link">
+                <img src={twitterLogo} alt="Twitter" />
+                <span className="social-text">Twitter</span>
+              </a>
+              <a href="#" className="social-link">
+                <img src={youtubeLogo} alt="YouTube" />
+                <span className="social-text">YouTube</span>
+              </a>
             </div>
-          </form>
-        </div>
-      </div>
-    </div>
-    <div className="container mx-auto">
-      <div className="flex flex-col md:flex-row py-5">
-        <div className="md:w-1/2 flex flex-col items-center">
-          <p className="text-lg font-bold uppercase mb-4">Connect</p>
-          <div className="flex space-x-4">
-            <a href="#" className="flex items-center space-x-2">
-              <img src={instagramLogo} alt="Instagram" className="w-6 h-6"/>
-              <span>Instagram</span>
-            </a>
-            <a href="#" className="flex items-center space-x-2">
-              <img src={facebookLogo} alt="Facebook" className="w-6 h-6"/>
-              <span>Facebook</span>
-            </a>
-            <a href="#" className="flex items-center space-x-2">
-              <img src={twitterLogo} alt="Twitter" className="w-6 h-6"/>
-              <span>Twitter</span>
-            </a>
-            <a href="#" className="flex items-center space-x-2">
-              <img src={youtubeLogo} alt="YouTube" className="w-6 h-6"/>
-              <span>YouTube</span>
-            </a>
           </div>
-        </div>
-        <div className="md:w-1/2 flex flex-col items-center mt-8 md:mt-0">
-          <p className="text-lg font-bold uppercase mb-4">More About Us</p>
-          <div className="flex space-x-4">
-            <a href="#" className="flex items-center space-x-2">
-              <i className="bi bi-question-circle-fill text-2xl"></i>
-              <span>FAQ's</span>
-            </a>
-            <a href="#" className="flex items-center space-x-2">
-              <i className="bi bi-info-circle-fill text-2xl"></i>
-              <span>About Us</span>
-            </a>
+          <div className="col-6 d-flex flex-column align-items-center">
+            <p className="lead text-uppercase">more about us</p>
+            <div className="social-links">
+              <a href="#" className="social-link">
+                <i className="bi bi-question-circle-fill"></i>
+                <span className="social-text">FAQ'S</span>
+              </a>
+              <a href="#" className="social-link">
+                <i className="bi bi-info-circle-fill"></i>
+                <span className="social-text text-uppercase">about us</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
-  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
